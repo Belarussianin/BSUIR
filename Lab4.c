94e7fa503636d31c7555d9b59165becbe9429cce
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2020-10-13 14:12:04 +0300

    Update Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -1,20 +1,19 @@
 /*#include <stdlib.h>
 #include <stdio.h>
 
-void bubble(int*);
+void bubble(int*, int);
 int* init(int*);
 void output(int*);
 
 int main() {
 	int mas[10]; // Obiavliaem massiv
 	init(mas);   // Zapolniaem massiv chislami ot -10 do 9
-	bubble(mas); // Sortiruem metodom bubbl'a
+	bubble(mas, 10); // Sortiruem metodom bubbl'a
 	output(mas); // Vivodim massiv
 	return 0;
 }
 
-void bubble(int* mas) {
-	int size = sizeof(mas);
+void bubble(int* mas, int size) {
 	for (int i = 0; i < size - 1; i++) {
 		for (int j = (size - 1); j > i; j--) {
 			if (mas[j - 1] > mas[j]) {
