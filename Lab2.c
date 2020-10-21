b42f693f9649823893f2faef736612a6abce4a9d
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2020-10-21 12:59:15 +0300

    Update Lab2.c

--- Lab2.c
+++ Lab2.c
@@ -3,13 +3,14 @@
 #include <windows.h>
 //050504 Кашевский Арсений
 //9. Рассортировать элементы главной диагонали матрицы 10x10 по возрастанию.
-const int size = 155; //Не ставить больше 155 для правильного вывода матрицы в консоли
-int matrix[size][size];
-int diag[size];
+#define SIZE 15 
+//Не ставить больше 155 для правильного вывода матрицы в консоли
+int matrix[SIZE][SIZE];
+int diag[SIZE];
 void init_matrix();
 void output_matrix();
 void sort_matrix();
-void sort(int*); //https://prog-cpp.ru/sort-bubble
+void sort(int*);
 
 int main() {
 	init_matrix();
@@ -19,18 +20,18 @@ int main() {
 }
 
 void init_matrix() {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
 			matrix[i][j] = rand() % 10;
 		}
-	} 
+	}
 }
 
 void sort(int* diag) {
 	// Для всех элементов
-	for (int i = 0; i < size - 1; i++)
+	for (int i = 0; i < SIZE - 1; i++)
 	{
-		for (int j = (size - 1); j > i; j--) // для всех элементов после i-ого
+		for (int j = (SIZE - 1); j > i; j--) // для всех элементов после i-ого
 		{
 			if (diag[j - 1] > diag[j]) // если текущий элемент меньше предыдущего
 			{
@@ -43,26 +44,26 @@ void sort(int* diag) {
 }
 
 void sort_matrix() {
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < SIZE; i++) {
 		diag[i] = matrix[i][i];
 	}
 	sort(diag);
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < SIZE; i++) {
 		matrix[i][i] = diag[i];
 	}
 }
 
 void output_matrix() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Для разноцветного вывода диагонали (д)
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Для разноцветного вывода главной диагонали в консоли.
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
 			if (i == j) {
-				SetConsoleTextAttribute(hConsole, rand() % 14 + 2); // (д)
+				SetConsoleTextAttribute(hConsole, rand() % 14 + 2); //  (меняет цвет вывода консоли)
 			}
 			printf(" %d", matrix[i][j]);
-			SetConsoleTextAttribute(hConsole, 15); // (д)
+			SetConsoleTextAttribute(hConsole, 15); //  (меняет цвет вывода консоли)
 		}
 		printf("\n");
 	}
-	SetConsoleTextAttribute(hConsole, 15); // (д)
+	SetConsoleTextAttribute(hConsole, 15); //  (меняет цвет вывода консоли)
 }
